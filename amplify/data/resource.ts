62de4b349b4518022fdea6af28c36a8f01c1e06f
fleagne <toshinori.s9252@gmail.com>
2024-10-22 20:49:18 +0900

    change defaultAuthorizationMode from apiKey to iam

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -19,11 +19,12 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
+    // defaultAuthorizationMode: "apiKey",
+    defaultAuthorizationMode: "iam",
     // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
+    // apiKeyAuthorizationMode: {
+    //   expiresInDays: 1,
+    // },
   },
 });
 
